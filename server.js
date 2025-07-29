ba587049366666cc487a287849ca7137ff944063
Masud Rahman <masud7866@gmail.com>
2025-07-29 14:15:07 +0600

    Cors update

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://faculty-status-display.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(
   session({
